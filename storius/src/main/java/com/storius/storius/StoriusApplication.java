bdda6e227b3268d056a73dd49d5da1448709c2d6
milksut <altaygokis@gmail.com>
2025-04-08 16:34:49 +0300

    Spring boot first commit

---
+++ storius/src/main/java/com/storius/storius/StoriusApplication.java
@@ -0,0 +1,13 @@
+package com.storius.storius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoriusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoriusApplication.class, args);
+	}
+
+}
---
+++ storius/src/test/java/com/storius/storius/StoriusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storius.storius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoriusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
